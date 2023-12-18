d75d2ac3a0c5417bdb87326c69777c627c08f5f2
Nathan Geronimo <nathangero@yahoo.com>
2023-12-18 12:25:09 -0800

    implement react router with Home and Error pages; remove default styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+
+  return (
+    <>
+      <h1>hello from home</h1>
+    </>
+  )
+}
