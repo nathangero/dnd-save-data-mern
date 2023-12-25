665ac11dcf3fed426642a96c627e17e0dd04c65a
Nathan Geronimo <nathangero@yahoo.com>
2023-12-24 22:53:59 -0800

    create User model

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  _id: String,
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 100
+  },
+  characters: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Character"
+    }
+  ]
+});
+
+const User = model("User", userSchema);
+module.exports = User;
