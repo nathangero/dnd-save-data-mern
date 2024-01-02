b564cac277835498ce21d33b900c7436f2f389f5
Nathan Geronimo <nathangero@yahoo.com>
2024-01-01 22:50:58 -0800

    add chunk size warning limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 300,
+  },
   server: {
     port: 3000,
     open: true,
