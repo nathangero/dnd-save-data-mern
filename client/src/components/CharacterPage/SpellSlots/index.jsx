05812a98c9038bb8b93b37d01893ba8e1ecdd14c
Nathan Geronimo <nathangero@yahoo.com>
2024-01-08 16:12:26 -0800

    implement showing spell slots

--- client/src/components/CharacterPage/SpellSlots/index.jsx
+++ client/src/components/CharacterPage/SpellSlots/index.jsx
@@ -1,9 +1,20 @@
 import "./style.css";
 import PropTypes from "prop-types";
 import { Character } from "../../../models/Character";
+import { SPELL_NAMES } from "../../../utils/enums";
 
 export default function SpellSlots(props) {
   const character = new Character(props.character);
+
+  /**
+ * Creates a div id from the feature/trait name
+ * @param {String} name Feature/Trait name
+ * @returns A string of the name lowercased and spaces replacecd with dashes 
+ */
+  const makeIdFromSpellSlot = (name) => {
+    const id = `spell-slot-${name.toLowerCase().split("_").join("-")}`;
+    return id;
+  }
 
   return (
     <div className="fs-3">
@@ -22,7 +33,20 @@ export default function SpellSlots(props) {
       </div>
 
       <div id="character-view-spell-slots" className="collapse show">
-        PLACEHOLDER
+        {Object.keys(character.spellSlots)?.map((item, index) => (
+          <>
+            {index < 2 ? null : // Ignore _typename and _id
+              <div key={index} id={makeIdFromSpellSlot(item)} className="d-flex justify-content-between">
+                {!character.spellSlots[item] ? null :
+                  <>
+                    <p>{SPELL_NAMES[item]}</p>
+                    <b>{character.spellSlots[item].current}/{character.spellSlots[item].max}</b>
+                  </>
+                }
+              </div>
+            }
+          </>
+        ))}
       </div>
     </div>
   )
