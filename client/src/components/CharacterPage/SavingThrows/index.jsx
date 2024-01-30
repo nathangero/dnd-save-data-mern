7e41769fa708a372aac95c29500c9403987f5349
Nathan Geronimo <nathangero@yahoo.com>
2024-01-30 11:43:41 -0800

    add saving throw editing initial rendering

--- client/src/components/CharacterPage/SavingThrows/index.jsx
+++ client/src/components/CharacterPage/SavingThrows/index.jsx
@@ -1,11 +1,58 @@
 import "./style.css";
 import PropTypes from "prop-types";
-import { Character } from "../../../models/Character";
 import { ABILITY_SCORE_KEYS, ABILITY_SCORE_NAMES, CHARACTER_VIEW_ID } from "../../../utils/enums";
 import { calcScoreWithProficiency } from "../../../utils/shared-functions";
 
 export default function SavingThrows({ char, toggleSectionShowing, isShowingSavingThrows, toggleEditing, isEditing }) {
   const character = { ...char }
+
+  const onClickProficient = (score) => {
+    console.log("@onClickProficient")
+    console.log("score:", score);
+  }
+
+  const renderEditing = () => {
+    return (
+      <ul className="list-unstyled">
+        {Object.values(ABILITY_SCORE_KEYS).map((score, index) => (
+          <li key={index} className="mb-3 stat-row">
+            <div className="d-flex">
+              <div className="me-3">
+                {character.savingThrows[score] ?
+                  <i className="bi bi-p-square editing" onClickCapture={() => onClickProficient(score)}></i> :
+                  <i className="bi bi-app"></i>
+                }
+              </div>
+              <p className="mb-0">{ABILITY_SCORE_NAMES[score]}</p>
+            </div>
+            <b>{calcScoreWithProficiency(character.scores[score], character.level, character.savingThrows[score], true)}</b>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  const renderViewing = () => {
+    return (
+      <ul className="list-unstyled">
+        {Object.values(ABILITY_SCORE_KEYS).map((score, index) => (
+          <li key={index} className="mb-3 stat-row">
+            <div className="d-flex">
+              <div className="me-3">
+                {character.savingThrows[score] ?
+                  <i className="bi bi-p-square viewing"></i> :
+                  <i className="bi bi-app"></i>
+                }
+              </div>
+              <p className="mb-0">{ABILITY_SCORE_NAMES[score]}</p>
+            </div>
+            <b>{calcScoreWithProficiency(character.scores[score], character.level, character.savingThrows[score], true)}</b>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
 
   return (
     <div className="fs-3">
@@ -23,23 +70,13 @@ export default function SavingThrows({ char, toggleSectionShowing, isShowingSavi
         <button className="btn btn-secondary button-edit" onClick={() => toggleEditing()}>{isEditing ? "Finish" : "Edit"}</button>
       </div>
 
+      {isEditing ?
+        renderEditing() :
+        renderViewing()
+      }
+
       <div id={CHARACTER_VIEW_ID.SAVING_THROWS} className="collapse show">
-        <ul className="list-unstyled">
-          {Object.values(ABILITY_SCORE_KEYS).map((score, index) => (
-            <li key={index} className="mb-3 stat-row">
-              <div className="d-flex">
-                <div className="me-3">
-                  {character.savingThrows[score] ?
-                    <i className="bi bi-p-square"></i> :
-                    <i className="bi bi-app"></i>
-                  }
-                </div>
-                <p className="mb-0">{ABILITY_SCORE_NAMES[score]}</p>
-              </div>
-              <b>{calcScoreWithProficiency(character.scores[score], character.level, character.savingThrows[score], true)}</b>
-            </li>
-          ))}
-        </ul>
+
       </div>
     </div>
   )
