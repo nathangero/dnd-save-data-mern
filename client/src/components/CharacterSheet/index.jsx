58f6f5975125d159a92e7fadd6dd00027aa1fc1c
Nathan Geronimo <nathangero@yahoo.com>
2024-02-15 13:35:59 -0800

    implement saving throws UI

--- client/src/components/CharacterSheet/index.jsx
+++ client/src/components/CharacterSheet/index.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 export default function CharacterSheet() {
 
   return (
-    <section className="character-sheet m-3 p-3">
+    <section className="character-sheet m-3 p-2">
       <h2>Dungeons & Dragons 5E Character Sheet</h2>
 
       <div className="sheet-header row-sm">
@@ -58,7 +58,7 @@ export default function CharacterSheet() {
       <br />
 
       <div className="sheet-body row-lg">
-        <div className="d-flex flex-row col-lg-4">
+        <div className="column-one d-flex flex-row col-lg-4">
           <div className="character-scores col-lg pe-2">
             <div className="score-container">
               <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.STR]}</h5>
@@ -91,14 +91,68 @@ export default function CharacterSheet() {
               <input type="number" className="edit-input fs-5" />
             </div>
           </div>
-          <div className="character-skills col-lg-7 border">
-            skills
+
+          <div className="d-flex flex-column col-lg-7 pe-2 border">
+            <div className="d-flex justify-content-between align-items-baseline">
+              <h5>Inspiration</h5>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="d-flex justify-content-between align-items-baseline">
+              <h5>Proficiency Bonus</h5>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+
+            <div className="character-saving-throws">
+              <h5 className="text-center">Saving Throws</h5>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.STR]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.DEX]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.CON]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.INT]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.WIS]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+              <div className="container-saving-throws">
+                <h6>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.CHA]}</h6>
+                <div className="d-flex align-items-center">
+                  <h6 className="me-2">mod</h6>
+                  <i className="bi bi-p-square fs-3"></i>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="character-scores col-lg-4 border">
+        <div className="column-two col-lg-4 border">
           armor
         </div>
-        <div className="character-scores col-lg-4 border">
+        <div className="column-three col-lg-4 border">
           feats
         </div>
       </div>
