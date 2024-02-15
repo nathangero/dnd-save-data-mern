4fad8314cc702fda692c596d011f875af3336d52
Nathan Geronimo <nathangero@yahoo.com>
2024-02-15 13:12:05 -0800

    implement ui for ability scores

--- client/src/components/CharacterSheet/index.jsx
+++ client/src/components/CharacterSheet/index.jsx
@@ -1,3 +1,4 @@
+import { ABILITY_SCORE_KEYS, ABILITY_SCORE_NAMES } from "../../utils/enums";
 import "./style.css";
 
 export default function CharacterSheet() {
@@ -6,7 +7,7 @@ export default function CharacterSheet() {
     <section className="character-sheet m-3 p-3">
       <h2>Dungeons & Dragons 5E Character Sheet</h2>
 
-      <div className="character-header row-sm">
+      <div className="sheet-header row-sm">
         <div className="character-info col-sm-3">
           <div>
             <label>Character Name</label>
@@ -51,6 +52,54 @@ export default function CharacterSheet() {
             <br />
             <input />
           </div>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="sheet-body row-lg">
+        <div className="d-flex flex-row col-lg-4">
+          <div className="character-scores col-lg pe-2">
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.STR]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.DEX]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.CON]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.INT]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.WIS]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+            <div className="score-container">
+              <h5>{ABILITY_SCORE_NAMES[ABILITY_SCORE_KEYS.CHA]}</h5>
+              <h6>score mod</h6>
+              <input type="number" className="edit-input fs-5" />
+            </div>
+          </div>
+          <div className="character-skills col-lg-7 border">
+            skills
+          </div>
+        </div>
+        <div className="character-scores col-lg-4 border">
+          armor
+        </div>
+        <div className="character-scores col-lg-4 border">
+          feats
         </div>
       </div>
     </section>
